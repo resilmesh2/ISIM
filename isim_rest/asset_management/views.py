e9c2110df780ad4dbc419e356732fe22ae7ca972
Michal Čech <445431@mail.muni.cz>
2024-10-30 15:55:36 +0100

    basic validations on missions

--- isim_rest/asset_management/views.py
+++ isim_rest/asset_management/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view  # type: ignore
 from rest_framework.response import Response
 
-from isim_rest.asset_management.data_formats.input_dtos import AssetListInputDTO
+from isim_rest.asset_management.data_formats.input_dtos import AssetListInputDTO, MissionListInputDTO
 from isim_rest.asset_management.data_formats.serde_utils import dec_hook_ip, enc_hook_ip
 from isim_rest.neo4j_rest.config import AppConfig
 from neo4j_adapter.rest_adapter import RESTAdapter
@@ -51,14 +51,18 @@ def mission(request: HttpRequest) -> Response:
     if request.method == "GET":  # type: ignore
         limit = get_limit(request)
         return Response(client.get_all_mission(limit))
-    properties = request.data  # type:  ignore
+    request_body = request.body
     try:
-        data = json.dumps(properties)
-        return Response(client.create_missions_and_components_string(data))
+        data = msgspec.json.decode(request_body, type=MissionListInputDTO, dec_hook=dec_hook_ip)
+        json_string = json.dumps(json.loads(msgspec.json.encode(data, enc_hook=enc_hook_ip)))
+        client.create_missions_and_components_string(json_string)
+    except ValidationError as e:
+        return Response(f"Bad input: {e!s}", status=status.HTTP_422_UNPROCESSABLE_ENTITY)
     except (ClientError, TransientError, DatabaseError) as e:
-        return Response("Exception on neo4j side, set operation failed. " + str(e), status=status.HTTP_400_BAD_REQUEST)
-    except (KeyError, TypeError):
-        return Response("Structured data was not provided or are incorrect.", status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            "Exception on neo4j side, set operation failed. " + str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    return Response("Processed successfully", status=status.HTTP_201_CREATED)
 
 
 @api_view(["POST"])
@@ -122,17 +126,20 @@ def applications(request: HttpRequest) -> Response:
     offset = get_offset(request)
     return Response(client.get_applications(limit=limit, offset=offset), status=status.HTTP_200_OK)
 
+
 @api_view(["GET"])
 def cves(request: HttpRequest) -> Response:
     limit = get_limit(request)
     offset = get_offset(request)
     return Response(client.get_all_cve(limit=limit, offset=offset), status=status.HTTP_200_OK)
 
+
 @api_view(["GET"])
 def cve(request: HttpRequest, cve_id: str) -> Response:
     limit = get_limit(request)
     offset = get_offset(request)
     return Response(client.get_cve(cve_id=cve_id, limit=limit, offset=offset), status=status.HTTP_200_OK)
+
 
 @api_view(["GET"])
 def ip_cves(request: HttpRequest, ip: str) -> Response:
