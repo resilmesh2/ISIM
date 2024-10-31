cb9a70c41385de38464544909fef6d66beba9b15
Michal Čech <445431@mail.muni.cz>
2024-10-31 10:19:50 +0100

    formatting, add logger

--- isim_rest/asset_management/apps.py
+++ isim_rest/asset_management/apps.py
@@ -3,11 +3,14 @@ import json
 import msgspec
 from django.apps.config import AppConfig
 from neo4j.exceptions import ClientError
+from structlog import getLogger
 
 from isim_rest.asset_management.data_formats.input_dtos import AssetListInputDTO
 from isim_rest.asset_management.data_formats.serde_utils import dec_hook_ip, enc_hook_ip
 from isim_rest.neo4j_rest.config import AppConfig as ISIMConfig
 from neo4j_adapter.rest_adapter import RESTAdapter
+
+logger = getLogger()
 
 
 class AssetManagementConfig(AppConfig):
@@ -30,7 +33,8 @@ class AssetManagementConfig(AppConfig):
         try:
             client.init_db()
         except ClientError as e:
-            print(e)  # todo replace with logging
+            if not "An equivalent constraint already exists" in e.message:
+                logger.exception(e)
         data = msgspec.convert(initial_data, type=AssetListInputDTO, dec_hook=dec_hook_ip)
         json_string = json.dumps(json.loads(msgspec.json.encode(data, enc_hook=enc_hook_ip)))
         client.store_assets(json_string)
