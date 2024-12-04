c58218a2a746c9dbcfdb35c5394e331a235c4c6f
Michal Čech <445431@mail.muni.cz>
2024-12-04 14:23:06 +0100

    static files included

--- isim_rest/neo4j_rest/urls.py
+++ isim_rest/neo4j_rest/urls.py
@@ -3,6 +3,7 @@ from django.urls.resolvers import RoutePattern, URLPattern, URLResolver
 
 from isim_rest.asset_management.urls import urlpatterns as asset_management_urls
 from isim_rest.neo4j_rest import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns: list[RoutePattern | URLResolver | URLPattern] = []
 
@@ -11,3 +12,5 @@ urlpatterns += asset_management_urls
 urlpatterns.append(
     path("", views.index, name="index"),
 )
+
+urlpatterns += staticfiles_urlpatterns()
