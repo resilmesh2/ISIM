90295411e6e2bed6bc2f6c33a6df4743ea7881bc
Milan Bohacek <bohacek@ics.muni.cz>
2026-02-13 19:13:27 +0100

    move config to root folder, update neo4j config and graphql schema.graphql

--- config.py
+++ config.py
@@ -13,8 +13,10 @@ CONF_DIR = BASE_DIR.parent / "config"
 @dataclass
 class Neo4jConfig:
     password: str
+    url: str
     bolt: str = "bolt://resilmesh-sap-neo4j:7687"
     user: str = "neo4j"
+
 
 
 @dataclass
